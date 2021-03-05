868da1d854232a90081efadcce764d3b2d76aa72
Ugur DOKMECI <ugurdokmeci@gmail.com>
2021-03-05 14:28:07 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+yii2tech / file-storage Minio Storage
+=====================================
+Adds Minio Storage to yii2tech / file-storage
+
+Installation
+------------
+
+The preferred way to install this extension is through [composer](http://getcomposer.org/download/).
+
+Either run
+
+```
+php composer.phar require --prefer-dist udokmeci/yii2-tech-filestorage-minio "*"
+```
+
+or add
+
+```
+"udokmeci/yii2-tech-filestorage-minio": "*"
+```
+
+to the require section of your `composer.json` file.
+
+
+Usage
+-----
+
+Once the extension is installed, simply use it in your code by  :
+
+```php
+<?= \udokmeci\yii2techfilestorage\AutoloadExample::widget(); ?>```
---
+++ src/Minio.php
@@ -0,0 +1,66 @@
+<?php
+
+
+namespace udokmeci\yii2techfilestorage\storage;
+
+use Aws\S3\S3Client;
+use Aws\Credentials\Credentials;
+use yii\base\InvalidConfigException;
+use yii2tech\filestorage\BaseStorage;
+
+class Minio extends \yii2tech\filestorage\amazon\Storage
+{
+    public $enableV4=false;
+    private $_amazonS3;
+    public $endpoint='http://0.0.0.0:9000';
+    public $bucketClassName = 'udokmeci\yii2techfilestorage\storage\Bucket';
+
+
+  /**
+     * @var string Amazon Bucket Region
+     */
+    public $region = 'eu-west-1';
+
+    /**
+     * @var string Amazon Version
+     */
+    public $version='latest';
+
+
+    /**
+      * @return S3Client Amazon S3 client instance.
+      */
+    public function getAmazonS3()
+    {
+        if (!is_object($this->_amazonS3)) {
+            $this->_amazonS3 = $this->createMinio();
+        }
+        return $this->_amazonS3;
+    }
+
+    /**
+     * Initializes the instance of the Amazon S3 service gateway.
+     * @return S3Client Amazon S3 client instance.
+     */
+    protected function createMinio()
+    {
+        $credentials = new Credentials($this->awsKey, $this->awsSecretKey);
+        
+        $settings=[
+            'version' =>  $this->version,
+            'region'  => $this->region,
+            'endpoint' => $this->endpoint,
+            'use_path_style_endpoint' => true,
+            'credentials' => [
+                'key'    => $this->awsKey,
+                'secret' => $this->awsSecretKey,
+            ],
+        ];
+         if ($this->enableV4) {
+            $settings['signature_version']='v4';
+        }
+
+        
+        return new S3Client($settings);
+    }
+}
